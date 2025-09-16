6dc4a3a6bf8d0c9b5f43ceea38b8f4596329c550
LedezmaVictoria <142950675+VictoriaLedezma@users.noreply.github.com>
2025-09-16 12:43:25 -0300

    feat(boot): proyecto Spring Boot generado desde Initializr

---
+++ src/test/java/ar/edu/utn/recti/RectiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.recti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
